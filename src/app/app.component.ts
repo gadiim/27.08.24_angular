d4bab8201e6fd4da15aa742c9c4edea42cb72bd9
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-09-03 17:41:52 +0300

    task 1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, SkillsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '27.08.24_ngModel';
+  cv = [
+    {
+      name: 'Conrad',
+      surname: 'Kellogg',
+      phone: '+123456789',
+      email: 'conrad.kellogg@fallout4.com',
+      residence: 'Diamond City, Commonwealth',
+      experience: 'Mercenary, Assassin, Former Institute Operative',
+      skills: 'Combat, Stealth, Strategy, Negotiation',
+    }]
 }
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css'
+})
+export class SkillsComponent {
+
+  @Input() name: string = '';
+  @Input() surname: string = '';
+  @Input() phone: string = '';
+  @Input() email: string = '';
+  @Input() residence: string = '';
+  @Input() experience: string = '';
+  @Input() skills: string = '';
+
+  selectedObject: string = '';
+
+  showCombat() {
+    this.selectedObject = 'combat';
+  }
+
+  showStealth() {
+    this.selectedObject = 'stealth';
+  }
+
+  showStrategy() {
+    this.selectedObject = 'strategy';
+  }
+
+  showNegotiation() {
+    this.selectedObject = 'negotiation';
+  }
+}
